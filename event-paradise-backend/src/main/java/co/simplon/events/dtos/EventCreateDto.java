def4c3c80b4199d39d2c8c90326fbea119dd4b64
DamlaSub <mme.dalloway@gmail.com>
2023-04-17 23:09:53 +0200

    add validation

--- event-paradise-backend/src/main/java/co/simplon/events/dtos/EventCreateDto.java
+++ event-paradise-backend/src/main/java/co/simplon/events/dtos/EventCreateDto.java
@@ -2,23 +2,40 @@ package co.simplon.events.dtos;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import co.simplon.events.entities.Location;
 import co.simplon.events.entities.Topic;
 
 public class EventCreateDto {
-	
+
 	private Long id;
 	
+	@NotNull
+	@Size(max=255)
 	private String name;
 	
+	@NotNull
+	@FutureOrPresent
 	private LocalDate date;
-
+	
+	@NotNull
 	private Long topicId;
 	
+	@NotNull
 	private Long locationId;
 	
+	@NotNull
+	@Positive
 	private Double price;
 	
+	@NotNull
+	@Size(max=1000)
 	private String description;
 
 	public EventCreateDto() {
